72e01cea4843a8a25a13ef15e62b167e4127cdce
細井翔太 <shouta.1167@icloud.com>
2026-04-08 23:21:30 +0900

    Time: 0 ms (100%), Space: 19.4 MB (13.26%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,6 +1,11 @@
-
-
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         return chr(sum(map(ord, t)) - sum(map(ord, s)))
+#できたら上の
+
+from collections import Counter
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        diff = Counter(t) - Counter(s)
+        return ''.join(diff.elements())
 
